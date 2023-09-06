296743b413493e050be4c5067496d98ec04ece84
Patience Otuke <patienceotuke@gmail.com>
2023-09-06 20:59:33 +0300

    Removed f strings

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 8):
     for b in range(a + 1, 10):
-        print(f"{a:d}{b:d}", end=", ")
-print(f"{a + 1:d}{b:d}")
+        print("{:d}{:d}".format(a, b), end=", ")
+print("{:d}{:d}".format(a + 1, b))
 
