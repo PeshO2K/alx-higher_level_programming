d2d9965e06a1c27a9bde5ef1884fb990bb10bd95
PeshO2K <patienceotuke@gmail.com>
2023-09-07 12:29:08 -0700

    Algo update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,26 +3,31 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 argc = len(argv)
 res = 0
+msg_1 = "Unknown operator. Available operators: +, -, * and /"
+
 
 def print_err(msg):
+
     print(msg)
     exit(1)
+
+
 if __name__ == "__main__":
+
     if argc == 4:
         a = int(argv[1])
         b = int(argv[3])
         op = argv[2]
-        match op:
-            case "+":
-                res = add(a, b)
-            case "-":
-                res = sub(a, b)
-            case "*":
-                res = mul(a, b)
-            case "/":
-                res = div(a, b)
-            case _:
-                print_err("Unknown operator. Available operators: +, -, * and /")
+        if op == "+":
+            res = add(a, b)
+        elif op == "-":
+            res = sub(a, b)
+        elif op == "*":
+            res = mul(a, b)
+        elif op == "/":
+            res = div(a, b)
+        else:
+            print_err(msg_1)
         print(f"{a:d} {op:s} {b:d} = {res:d}")
     else:
         print_err("Usage: ./100-my_calculator.py <a> <operator> <b>")
