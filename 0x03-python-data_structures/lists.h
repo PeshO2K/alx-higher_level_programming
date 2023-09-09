e1283387197424f8a526abf04a89ec29ec66e3bc
Patience Otuke <patienceotuke@gmail.com>
2023-09-09 21:28:14 +0300

    Header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int list_length(listint_t **head);
 
 #endif /* LISTS_H */
