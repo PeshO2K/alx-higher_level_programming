ae93ef1ec8f9b53e09474be2676eec4c50ef789e
Patience Otuke <patienceotuke@gmail.com>
2023-09-13 17:33:09 +0300

    Tasks 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    if matrix:
-        new_matrix = [[element**2 for element in row] for row in matrix]
-    return new_matrix
+    return [[element**2 for element in row] for row in matrix]
