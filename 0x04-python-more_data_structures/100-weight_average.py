4b3dcb660555ef3feb8eb35a9188473e21a64efd
Patience Otuke <patienceotuke@gmail.com>
2023-09-13 18:31:47 +0300

    Advanced tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total_weight = sum([score * weight for score, weight in my_list])
+    sum_weights = sum([weight for _, weight in my_list])
+    return total_weight / sum_weights
