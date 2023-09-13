0963b821c20b5ea50436d231438bf0137159a44c
Patience Otuke <patienceotuke@gmail.com>
2023-09-13 18:44:29 +0300

    TAsk 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda elem: list(map(lambda x: x**2, elem)), matrix))
+    return [[elem**2 for elem in row] for row in matrix]
+
