2768c13726a26601bb465990ca0d74dc394c63fe
Patience Otuke <patienceotuke@gmail.com>
2023-10-10 04:45:38 +0300

    Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-Module rectangle
-"""
+"""  module rectangle"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    class rectangle
-    """
-    def __init__(self, size):
-        """
-        constructor rectangle
-        """
-        super().integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+    """ class rectangle"""
 
-    def area(self):
-        """
-        area
-        """
-        return super().area()
+    def __init__(self, size):
+        """init rectangle"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """describe rectangle"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
