bc17c1595e282a32920869c014677f0b25a7bee8
Patience Otuke <patienceotuke@gmail.com>
2023-10-10 04:22:53 +0300

    Handled bool

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,6 +19,6 @@ class BaseGeometry():
         Function validates value
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
