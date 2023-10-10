b68ea67fa86915332081152e7b3d572f21fc1e18
Patience Otuke <patienceotuke@gmail.com>
2023-10-10 03:25:59 +0300

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for check if object and
+class are exactly the same
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    compares class and object
+    """
+
+    return isinstance(obj, a_class)
