744c7b66c6b25c3befece247bddee0c81276c4a2
Patience Otuke <patienceotuke@gmail.com>
2023-10-11 21:09:04 +0300

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module class to JSON"""
+
+
 def class_to_json(obj):
     """Function that converts class to JSON"""
     return {key: value for key, value in obj.__dict__.items()
