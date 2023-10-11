95011c7a04e5154172829c5e0dc68ff2b2f9631f
Patience Otuke <patienceotuke@gmail.com>
2023-10-11 19:31:25 +0300

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module returns Json
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function to return JSON of object
+    """
+    return json.dumps(my_obj)
