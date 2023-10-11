ba3a19b4abbb9f51aac37ce7a23291eb86ee0a76
Patience Otuke <patienceotuke@gmail.com>
2023-10-11 21:26:49 +0300

    Task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to file, after each line
+    containing a specific string
+    """
+    with open(filename, 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if search_string in line:
+                lines[i] = line + new_string + '\n'
+        f.seek(0)
+        f.writelines(lines)
