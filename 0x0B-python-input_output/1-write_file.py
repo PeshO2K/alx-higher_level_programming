f60225d1404514f50cc91587ab072e374c352d5c
Patience Otuke <patienceotuke@gmail.com>
2023-10-11 18:59:18 +0300

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
     Function to write to file
     Overwrite contents if it exists
     """
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
