a040d37ceb2b19c42f7303d834e57dcba4a66724
Patience Otuke <patienceotuke@gmail.com>
2023-10-11 18:47:58 +0300

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     function to read file
     """
-    with open(filename, encoding="utf-8") as file:
-        read_data = file.read()
-        print(read_data)
+    with open(filename, encoding="utf-8",) as file:
+        for line in file:
+            print(line, end="")
