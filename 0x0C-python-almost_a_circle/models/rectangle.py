69a8cdb31fdfeb1be4549dd87057ec244aa367e5
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 17:46:04 +0300

    Task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,7 +111,7 @@ class Rectangle(Base):
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
                 f" - {self.__width}/{self.__height}")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update attributes"""
         attributes = ['id', 'width', 'height', 'x', 'y']
         if args and len(args) > 0:
