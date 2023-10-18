72319189be02cfc21f98de76a7bdb1dae7b6abbc
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 18:49:40 +0300

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,17 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor func"""
+        self.size = size        
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get size"""
+        return self.width
+    @size.setter
+    def size(self, value):
+        """Set szie"""
+        self.width = self.height = value
 
     def __str__(self):
         """Print Square"""
