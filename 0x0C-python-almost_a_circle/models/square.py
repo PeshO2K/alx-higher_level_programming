dd30e60f5062e50afe1f3e4af69322dbecea668b
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 18:42:29 +0300

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Sqaure Module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Print Square"""
+        sqr = "[Square] ({}) {}/{} - {}"
+        return sqr.format(self.id, self.x, self.y, self.width)
