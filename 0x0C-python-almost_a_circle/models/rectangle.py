920dae9b5f909260042ec772c45fee3f37454552
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 15:11:48 +0300

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """
 Rectangle module
 """
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -18,34 +19,58 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        """
+        get width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """
+        set width
+        """
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        """
+        get height
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """
+        set height
+        """
+        self.__height = value
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        """
+        get x
+        """
+        return self.__x
 
-        @y.setter
-        def x(self, value):
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """
+        set x
+        """
+        self.__x = value
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        """
+        get y
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """
+        set y
+        """
+        self.__y = value
