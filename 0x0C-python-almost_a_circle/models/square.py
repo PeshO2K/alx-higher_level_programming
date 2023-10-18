363860849e8f341524d76b21cb86960a5622c30a
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 19:52:36 +0300

    Square tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor func"""
-        self.size = size        
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
@@ -25,3 +25,15 @@ class Square(Rectangle):
         """Print Square"""
         sqr = "[Square] ({}) {}/{} - {}"
         return sqr.format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Update attributes"""
+        if args and len(args) > 0:
+            args = list(args)
+            if len(args) > 1:
+                args.insert(2, args[1])
+            super().update(*args)
+        else:
+            if 'size' in kwargs:
+                kwargs['width'] = kwargs['height'] = kwargs['size']
+            super().update(**kwargs)
