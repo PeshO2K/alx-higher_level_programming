c712ae72af85fd337c366872ca7417617b911fbb
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 12:51:04 +0300

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Base class Module
+"""
+
+class Base():
+	"""Base class"""
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		"""Constrctor function
+		"""
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = Base.__nb_objects
+
+		
