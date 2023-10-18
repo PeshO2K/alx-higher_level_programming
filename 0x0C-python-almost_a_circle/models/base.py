27540b4b9bfc940003b43e71a641a35bdc9fae99
Patience Otuke <patienceotuke@gmail.com>
2023-10-18 21:15:14 +0300

    Task 15 -task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base class Module
 """
+import json
 
 
 class Base():
@@ -16,3 +17,52 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        return JSON rep of dictionaries
+        """
+        if not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write json to file"""
+        list_dicts = [obj.to_dictionary() for obj in list_objs]\
+            if list_objs else []
+
+        with open(cls.__name__ + '.json', "w") as file:
+            file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """From Json"""
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create preinitialized class"""
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Load from files"""
+        filename = cls.__name__ + '.json'
+        if not os.path.exists(filename):
+            return []
+
+        with open(filename, 'r') as file:
+            list_dicts = cls.from_json_string(file.read())
+
+        return [cls.create(**dict_) for dict_ in list_dicts]
