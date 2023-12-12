13c8714a0c0ce5272a075a28ba93fb1c2ce87c16
Patience Otuke <patienceotuke@gmail.com>
2023-12-12 03:42:21 +0300

    Corrections and advanced task 1

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (arg1 !== undefined && arg2 !== undefined) {
-  console.log(arg1 + ' is ' + arg2);
-} else {
-  console.log('Please provide two arguments.');
-}
+const argv = process.argv;
+console.log(`${argv[2]} is ${argv[3]}`);
