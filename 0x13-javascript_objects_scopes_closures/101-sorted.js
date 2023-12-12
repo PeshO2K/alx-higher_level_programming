942df9a632ca992ac0908bef02b1922c1f1f91f5
Patience Otuke <patienceotuke@gmail.com>
2023-12-12 22:05:20 +0300

    Advanced tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { dict } = require('./101-data.js');
+const newDict = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+  if (newDict[occurrence]) {
+    newDict[occurrence].push(userId);
+  } else {
+    newDict[occurrence] = [userId];
+  }
+}
+
+console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const fs = require('fs');
+const path1 = process.argv[2];
+const path2 = process.argv[3];
+const destination = process.argv[4];
+
+fs.readFile(path1, 'utf8', (err, data1) => {
+  if (err) throw err;
+  fs.readFile(path2, 'utf8', (err, data2) => {
+    if (err) throw err;
+    fs.writeFile(destination, data1 + data2, (err) => {
+      if (err) throw err;
+    });
+  });
+});
