aa568f45e3e05b5583a97611c33ac7ae6963e376
Patience Otuke <patienceotuke@gmail.com>
2023-12-18 01:03:06 +0300

    Advanced Task 1

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Model City Module"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City Class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(120), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
