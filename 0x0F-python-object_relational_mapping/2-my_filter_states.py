892c70219da665f91561479fc777e0ba107d7dc0
Patience Otuke <patienceotuke@gmail.com>
2023-12-17 19:21:10 +0300

    TAsk 2 Using format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name = %s\
-                 ORDER BY id ASC", (sys.argv[4],))
+                 WHERE name = '{}'\
+                 ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
