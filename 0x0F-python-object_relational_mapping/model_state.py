28b768a08c5bbce2acf2439867c022c27e56a79d
Patience Otuke <patienceotuke@gmail.com>
2023-12-17 17:31:47 +0300

    Alchemy Task 1

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Model State Module"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State Class"""
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(120), nullable=False)
