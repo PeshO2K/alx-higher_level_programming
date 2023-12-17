f3a3562be8d2b325b5557e6079b91361685c9026
Patience Otuke <patienceotuke@gmail.com>
2023-12-17 19:36:02 +0300

    TAsk 2 Using BINARY

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name = '{}'\
+                 WHERE BINARY name = '{}'\
                  ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
