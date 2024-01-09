f0829f587d6a0226cc33f6e9fc720807ae5d2a5f
PeshO2K <patienceotuke@gmial.com>
2024-01-09 20:06:16 +0300

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send post request and display body of response
-curl -sd "email: test@gmail.com" -d "subject: I will always be here for PLD" $1
+curl -s -d "email: test@gmail.com" -d "subject: I will always be here for PLD" $1
