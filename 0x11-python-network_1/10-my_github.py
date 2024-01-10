04b27eed420e54344eb89afd0f7d71d393ba95a0
PeshO2K <patienceotuke@gmial.com>
2024-01-10 18:05:04 +0300

    Task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''Module to get github id'''
+import requests
+import sys
+
+
+def get_github(username, password):
+    '''Function to get github id'''
+    url = ' https://api.github.com/user'
+    data = (username, password)
+    response = requests.get(url, auth=data)
+    html = response.json()
+    print(html.get('id'))
+
+
+if __name__ == "__main__":
+    '''get github id'''
+    get_github(sys.argv[1], sys.argv[2])
