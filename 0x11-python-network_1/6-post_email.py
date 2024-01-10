9d1ea708d016a0683ef7462f9969e498f22fabf1
PeshO2K <patienceotuke@gmial.com>
2024-01-10 16:50:53 +0300

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import requests
+import sys
+
+
+def post_email(url, email):
+    '''Function to post email'''
+    data = {'email': email}
+    response = requests.post(url, data)
+    html = response.text
+    print(html)
+
+
+if __name__ == "__main__":
+    '''Post email'''
+    post_email(sys.argv[1], sys.argv[2])
