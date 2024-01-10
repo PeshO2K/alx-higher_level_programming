64ee3ad7b97c7ccb22fa72925e4f7957115bfdc1
PeshO2K <patienceotuke@gmial.com>
2024-01-10 17:11:13 +0300

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Module to handle http error'''
+import requests
+import sys
+
+
+def handle_http_error(url):
+    '''Function to handle http error'''
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+        print(response.text)
+    except Exception:
+        print('Error code:', response.status_code)
+
+
+if __name__ == "__main__":
+    '''Handle http errors'''
+    handle_http_error(sys.argv[1])
