82e433509dede7ce903b5529f8747d604db55fd3
PeshO2K <patienceotuke@gmial.com>
2024-01-10 16:35:53 +0300

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''Module to fetch request id'''
+import requests
+import sys
+
+
+def fetch_request_id(url):
+    '''Function to fetch url'''
+    header = "X-Request-Id"
+    response = requests.get(url)
+    req_id = response.headers.get(header)
+    print(req_id)
+
+
+if __name__ == "__main__":
+    '''Get body response'''
+    fetch_request_id(sys.argv[1])
