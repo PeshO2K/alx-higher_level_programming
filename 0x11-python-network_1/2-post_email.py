0174eb2cfabfc928b4cd50bae74d36cdf69958c5
PeshO2K <patienceotuke@gmial.com>
2024-01-10 15:31:35 +0300

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''Module to post email'''
+import urllib.request
+import urllib.parse
+import sys
+
+
+def post_email(url, email):
+    '''Function to post email'''
+    email = urllib.parse.urlencode({'email': email})
+    email = email.encode('ascii')
+    req = urllib.request.Request(url, email)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    '''Post email'''
+    post_email(sys.argv[1], sys.argv[2])
