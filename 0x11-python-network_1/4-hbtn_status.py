09324776d54a5ce7bf781d09a16af8241eb96e4e
PeshO2K <patienceotuke@gmial.com>
2024-01-10 16:25:43 +0300

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''Module the fetches https://alx-intranet.hbtn.io/status'''
+import requests
+
+
+def fetch_status():
+    '''Function to fetch url'''
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url).text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+
+
+if __name__ == "__main__":
+    '''Get body response'''
+    fetch_status()
