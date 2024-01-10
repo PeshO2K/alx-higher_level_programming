1c40241cfdeff08b87a6d00b03c5470bad392b1e
PeshO2K <patienceotuke@gmial.com>
2024-01-10 17:51:47 +0300

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''Module to post letter'''
+import requests
+import sys
+
+
+def post_letter(letter=""):
+    '''Function to post letter'''
+    url = 'http://0.0.0.0:5000/search_user'
+    lett = {'q': letter}
+    response = requests.post(url, data=lett)
+    try:
+        html = response.json()
+        if html:
+            print("[{}] {}".format(html.get('id'), html.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    '''Post letter'''
+    post_letter(sys.argv[1] if len(sys.argv) > 1 else "")
