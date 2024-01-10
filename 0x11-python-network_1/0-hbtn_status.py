23172e2a0e7da19d921d25a8495aa637adf38e5a
PeshO2K <patienceotuke@gmial.com>
2024-01-10 14:26:26 +0300

    Task 0: print page

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 '''Module the fetches https://alx-intranet.hbtn.io/status'''
+import urllib.request
 
 
 def fetch_url():
     '''Function to fetch url'''
-    import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html.decode('utf-8')))
+        print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
 
