6914a705a8214be9e4215172595f1ff20b087b2a
PeshO2K <patienceotuke@gmial.com>
2024-02-09 12:03:56 +0300

    Semistd

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,42 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    fetchAndPrintCharacterNames(characterUrls)
-      .then(() => {
-        // All character names have been printed
-      })
-      .catch(err => console.error(err));
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const number = process.argv[2];
+request(url, async (error, response, body) => {
+  if (!error) {
+    const films = JSON.parse(body).results;
+    //   console.log(people);
+    const keys = Object.keys(films);
+    const myFilm = films[keys[number - 1]];
+    for (const char of myFilm.characters) {
+      await new Promise((resolve) => {
+        request(char, (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+          }
+          resolve();
+        });
+      });
+    }
   }
 });
-
-function fetchAndPrintCharacterNames (characterUrls) {
-  return new Promise((resolve, reject) => {
-    let remainingRequests = characterUrls.length;
-
-    characterUrls.forEach(characterUrl => {
-      request.get(characterUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-
-          remainingRequests--;
-          if (remainingRequests === 0) {
-            resolve();
-          }
-        }
-      });
-    });
-  });
-}
