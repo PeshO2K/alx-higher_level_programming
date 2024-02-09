9f1129ea3dcdfd915a5e916342dbc8c00a0ff21c
PeshO2K <patienceotuke@gmial.com>
2024-02-09 11:49:55 +0300

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,11 +5,22 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body);
-    const wedgeMovies = filmsData.results.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-    console.log(wedgeMovies.length);
-  }
-});
+    if (error) {
+      console.error(error);
+    } else {
+      let filmsData;
+
+      try {
+        filmsData = JSON.parse(body).results || JSON.parse(body);
+      } catch (parseError) {
+        console.error(parseError);
+        return;
+      }
+
+      const wedgeMovies = filmsData.filter(movie =>
+        movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      );
+
+      console.log(wedgeMovies.length);
+    }
+  });
